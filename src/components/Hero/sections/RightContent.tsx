9e48dfb4db7801719cac0cab387b6bb3cba91ff0
mehedy hassan <dev.mehedy.hassan@gmail.com>
2023-03-14 01:46:24 +0600

    hero image issue check

--- src/components/Hero/sections/RightContent.tsx
+++ src/components/Hero/sections/RightContent.tsx
@@ -2,7 +2,7 @@ const RightContent = () => {
   return (
     <div className="">
       <img
-        src="/public/img/moon.png"
+        src="/img/moon.png"
         className="h-[600px] w-full animate-bouncing-slow"
         alt="astronot"
       />
