3ab6349f50c37023aadcbdb51e5205df3f40d558
mehedy hassan <mehedy@algosolver.com>
2023-03-16 02:31:43 +0600

    web right section added

--- src/components/Work/index.tsx
+++ src/components/Work/index.tsx
@@ -1,11 +1,16 @@
 import LeftContent from "./sections/LeftContent"
-import RightContent from "./sections/RightContent"
 
 const Work = () => {
   return (
     <div className="h-screen snap-center text-white flex gap-2 items-center justify-around">
       <LeftContent/>
-      <RightContent/>
+      <div>
+      <img
+        src="/img/web.png"
+        className="h-[600px] w-full animate-bouncing-slow"
+        alt="astronot"
+      />
+      </div>
     </div>
   )
 }
